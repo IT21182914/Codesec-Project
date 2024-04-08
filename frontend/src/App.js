ec987b122e9ec1ff60f5d18720fd43b214eed66c
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-04-08 14:57:51 +0530

    Change isLogin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./components/HomePage";
 import FavoritePage from "./components/FavoritePage";
 
 function App() {
-  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  // const isLoggedIn = useSelector((state) => state.isLoggedIn);
   return (
     <div>
       <Router>
@@ -14,8 +14,11 @@ function App() {
           <Route path="/" element={<LoginPage />}></Route>
           <Route path="/registration" element={<RegisterPage />}></Route>
 
-          {isLoggedIn && <Route path="/home" element={<HomePage />} />}
-          {isLoggedIn && <Route path="/favorite" element={<FavoritePage />} />}
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/favorite" element={<FavoritePage />}></Route>
+
+          {/* {isLoggedIn && <Route path="/home" element={<HomePage />} />}
+          {isLoggedIn && <Route path="/favorite" element={<FavoritePage />} />} */}
         </Routes>
       </Router>
     </div>
